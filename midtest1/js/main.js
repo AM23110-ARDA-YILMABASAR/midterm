aa3405c063ef25e72ef6968cad289eb7a5fbdf9c
AM23110-ARDA-YILMABASAR <am23110@shibaura-it.ac.jp>
2025-05-26 16:33:36 +0900

    deleted old tests

--- midtest1/js/main.js
+++
@@ -1,9 +0,0 @@
-// File: js/main.js
-
-// Toggle mobile navigation
-const navToggle = document.querySelector('.nav-toggle');
-const navList = document.querySelector('.main-nav ul');
-
-navToggle.addEventListener('click', () => {
-  navList.classList.toggle('show');
-});
