1a6a27bee3a46c641974cabf958913ec94af2783
AM23110-ARDA-YILMABASAR <am23110@shibaura-it.ac.jp>
2025-05-28 19:15:29 +0900

    only midterm

--- midtest1_nav_final491/js/main.js
+++
@@ -1,4 +0,0 @@
-// File: js/main.js
-const navToggle = document.querySelector('.nav-toggle');
-const navList = document.querySelector('.main-nav ul');
-navToggle.addEventListener('click', () => { navList.classList.toggle('show'); });
